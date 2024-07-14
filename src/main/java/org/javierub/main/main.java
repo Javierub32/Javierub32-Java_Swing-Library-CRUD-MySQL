740f107cae6c764c4ac8fdf6bce7379e41ea0d25
Javierub32 <javierub38@gmail.com>
2024-07-14 11:58:57 +0200

    Añado el proyecto, contiene dos JPanels y varios JButtons

---
+++ src/main/java/org/javierub/main/main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.javierub.main;
+
+import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialLighterIJTheme;
+import org.javierub.library.DashBoard;
+
+/**
+ *
+ * @author USAURIO
+ */
+public class main {
+    public static void main(String[] args) {
+        DashBoard LoginFrame = new DashBoard();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+}
